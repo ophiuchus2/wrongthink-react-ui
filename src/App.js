7ae53699f9441ea8d9226e88e23ef491601bc57b
ophiuchus2 <ophiuchus@gmx.com>
2020-09-08 02:00:51 -0500

    integrate grpc-web, add some client rpc calls

--- src/App.js
+++ src/App.js
@@ -19,8 +19,42 @@ import {
 } from 'semantic-ui-react';
 import SlackMessage from './SlackMessage';
 const ReactMarkdown = require('react-markdown');
+/*const { CreateUserRequest,
+        CreateWrongThinkChannelRequest,
+        CreateWrongthinkCommunityRequest,
+        GetWrongthinkChannelsRequest,
+        GetWrongthinkCommunitiesRequest,
+        GetWrongthinkMessagesRequest,
+        ListenWrongthinkMessagesRequest,
+        WrongthinkChannel,
+        WrongthinkCommunity,
+        WrongthinkMessage,
+        WrongthinkMeta,
+        WrongthinkUser } = require('./wrongthink_pb.js');
+const { WrongthinkClient } = require('./wrongthink_grpc_web_pb.js');*/
+//var grpc = require('grpc');
+var messages = require('./wrongthink_pb');
+var services = require('./wrongthink_grpc_web_pb');
 
-const VerticalSidebar = ({ animation, direction, visible }) => (
+var client = new services.wrongthinkClient('http://10.0.0.24:8080');
+
+/*
+function getWrongthinkChannels(communityid) {
+  var req = new messages.GetWrongthinkChannelsRequest([+communityid]);
+  console.log("making getWrongthinkChannels request:");
+  console.log(req.toObject());
+  var call = client.getWrongthinkChannels(req);
+  call.on('data', function(channel) {
+    console.log(channel.toObject());
+  });
+  call.on('error', function(e) {
+    // An error has occurred and the stream has been closed.
+    console.log(e);
+  });
+}
+*/
+
+/*const VerticalSidebar = ({ animation, direction, visible, channels }) => (
   <Sidebar
     as={Menu}
     animation={animation}
@@ -31,20 +65,11 @@ const VerticalSidebar = ({ animation, direction, visible }) => (
     visible={visible}
     width='thin'
   >
-    <Menu.Item as='a'>
-      # Channel 1
-    </Menu.Item>
-    <Menu.Item as='a'>
-      # Channel 2
-    </Menu.Item>
-    <Menu.Item as='a'>
-      # Channel 3
-    </Menu.Item>
-    <Menu.Item as='a'>
-      # Channel N
-    </Menu.Item>
+
+    {channels.map((channel, i) => <Menu.Item as='a'> {channel} </Menu.Item> )}
+
   </Sidebar>
-)
+)*/
 
 function exampleReducer(state, action) {
   switch (action.type) {
@@ -65,6 +90,7 @@ export default class App extends React.Component {
     super(props);
     this.state = {
       messages: [],
+      channels: [],
       visible: true
     }
 
@@ -73,26 +99,137 @@ export default class App extends React.Component {
   }
 
   componentDidMount() {
-    this.setState({
-      messages: [
-        {
-          _id: 1,
-          text: 'hello anon',
-          createdAt: new Date(),
+    var channels = [];
+
+    var me = this;
+    var chatmessages = [];
+    function getWrongthinkMessages() {
+      var req = new messages.GetWrongthinkMessagesRequest(
+        [/* channelid */  1,
+         /* limit */      0,
+         /* afterid */    0,
+         /* afterdate*/   0] );
+      console.log("making getWrongthinkMessages request:");
+      console.log(req.toObject());
+      var call = client.getWrongthinkMessages(req);
+
+      call.on('data', function(message) {
+        var mobj = message.toObject();
+        chatmessages.push({
+          _id: 1, /*need to include message id in the response from the server*/
+          text: mobj.text,
+          createdAt: new Date(), /*need to convert date int*/
           user: {
-            _id: 2,
-            name: 'Anonymous',
-            avatar: 'https://placeimg.com/140/140/any',
-          },
-        },
-      ],
-    })
+            _id: 1,
+            name: 'Anonymous'
+          }
+        });
+        console.log(chatmessages.toObject());
+      });
+
+      call.on('error', function(e) {
+        // An error has occurred and the stream has been closed.
+        console.log(e);
+      });
+
+      call.on('end', function(end) {
+        me.setState({
+          messages: chatmessages
+        });
+        var req = new messages.ListenWrongthinkMessagesRequest(
+          [/* uname */  'user1', /* hard code for now */
+           /* channelid */      1,
+           /* channelname */    'pol'] );
+        var call = client.listenWrongthinkMessages(req);
+        call.on('error', function(e) {
+          // An error has occurred and the stream has been closed.
+          console.log(e);
+        });
+        call.on('data', function(message) {
+          var chatmessages = [];
+          var mobj = message.toObject();
+          chatmessages.push({
+            _id: 1, /*need to include message id in the response from the server*/
+            text: mobj.text,
+            createdAt: new Date(), /*need to convert date int*/
+            user: {
+              _id: 1,
+              name: 'Anonymous'
+            }
+          });
+          me.setState(previousState => ({
+            messages: GiftedChat.append(previousState.messages, chatmessages),
+          }))
+          console.log(message.toObject());
+        });
+
+      });
+    }
+    // this takes as a parameter the envoy proxy url
+    function getWrongthinkChannels(communityid) {
+      var req = new messages.GetWrongthinkChannelsRequest([+communityid]);
+      console.log("making getWrongthinkChannels request:");
+      console.log(req.toObject());
+      var call = client.getWrongthinkChannels(req);
+      call.on('data', function(channel) {
+        channels.push(channel.getName());
+        console.log(channel.toObject());
+      });
+      call.on('error', function(e) {
+        // An error has occurred and the stream has been closed.
+        console.log(e);
+      });
+      call.on('end', function(end) {
+        me.setState({
+          channels: channels
+        });
+        getWrongthinkMessages();
+      });
+    }
+
+    getWrongthinkChannels(1);
+
   }
 
-  onSend(messages = []) {
-    this.setState(previousState => ({
-      messages: GiftedChat.append(previousState.messages, messages),
-    }))
+/*
+function createUser(un, pw, ad) {
+  var req = new messages.CreateUserRequest([un, pw, (ad == "true")]);
+  console.log("making request:");
+  console.log(req.toObject());
+  client.createUser(req, function(error, user) {
+    if (error) {
+      console.log(error);
+      return;
+    } else {
+      console.log('created user:');
+      console.log(user.toObject());
+    }
+  });
+}
+*/
+
+  onSend(chatmessages = []) {
+    console.log(chatmessages);
+    var text = chatmessages[0].text;
+    var req = new messages.WrongthinkMessage();
+    req.setUname('user1');
+    req.setChannelname('pol');
+    req.setChannelid(1);
+    req.setUserid(1);
+    req.setText(text);
+    console.log(this);
+    var me = this;
+    client.sendWrongthinkMessageWeb(req, null, function(error, meta) {
+      if(error) {
+        console.log(error);
+        return;
+      } else {
+        console.log('sent message');
+        /*me.setState(previousState => ({
+          messages: GiftedChat.append(previousState.messages, chatmessages),
+        }));*/
+      }
+    });
   }
 
   onToggle() {
@@ -146,11 +283,21 @@ export default class App extends React.Component {
         <Sidebar.Pushable as={Segment} style={{ overflow: 'hidden' }}>
 
           {!vertical && (
-            <VerticalSidebar
+
+            <Sidebar
+              as={Menu}
               animation={animation}
               direction={direction}
+              icon='labeled'
+              inverted
+              vertical
               visible={visible}
-            />
+              width='thin'
+            >
+
+              {this.state.channels.map((channel, i) => <Menu.Item as='a'> {channel} </Menu.Item> )}
+
+            </Sidebar>
           )}
 
           <Sidebar.Pusher dimmed={dimmed && visible}>
